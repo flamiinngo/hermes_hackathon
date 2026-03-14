fa800d7a706fdad00bc8b582def994b5a238ceac
flamiinngo <108100593+flamiinngo@users.noreply.github.com>
2026-03-14 12:03:49 +0100

    Add application entry point in main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+// src/main.jsx
+// FILE 11 of 13
+// Application entry point
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
